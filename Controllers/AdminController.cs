1c5d9e88460c3f258cac8e9531b20429934a590b
Aymen Bouazizi <aymendotbouazizi@gmail.com>
2019-12-08 19:54:30 +0100

    Added SportsStore Project

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository productRepository;
+        public AdminController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public ViewResult Products()
+        {
+            return View(productRepository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = productRepository.Products.FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                productRepository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Products");
+            }
+            else
+            {
+          // there is something wrong with the data values
+return View(product);
+            }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(int productId)
+        {
+            Product deletedProduct = productRepository.DeleteProduct(productId);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = string.Format("{0} was deleted",
+                deletedProduct.Name);
+            }
+            return RedirectToAction("Products");
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public int PageSize = 4;
+        private IProductRepository productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+
+        public ViewResult List(string category, int page = 1)
+        {
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+
+             Products = productRepository.Products
+            .Where(p => category == null || p.Category == category)
+            .OrderBy(p => p.ProductID)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize),
+
+             PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = category == null ?productRepository.Products.Count() :productRepository.Products.Where(e => e.Category == category).Count()
+             },
+             CurrentCategory = category
+            };
+            return View(model);
+        }
+    }
+}
